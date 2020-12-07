8606fa904a56ee3cc38ae4286bb3a97e73e4c101
Maxim Obelets <maximobelets@gmail.com>
2020-12-08 00:53:29 +0500

    Add basic routes

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function About() {
+	return (
+		<>
+			<h1>
+				About
+			</h1>
+			<button>
+				<Link href='/'>Home</Link>
+			</button>
+		</>
+	)
+}
---
+++ pages/layouts.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Layouts() {
+	return (
+		<>
+			<h1>
+				Layouts
+			</h1>
+			<button>
+				<Link href='/'>Home</Link>
+			</button>
+		</>
+	)
+}
